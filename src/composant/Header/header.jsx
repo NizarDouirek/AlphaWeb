bf898f6a9f42ef230a5b2eb7d6eb3d6537273d19
douireknizar <douireknizar@gmail.com>
2025-05-26 22:52:11 +0200

    add Header

--- src/composant/Header/header.jsx
+++ src/composant/Header/header.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import './header.css';
-export default function Header(){
-    return(
-        <div className="header">
-           <div className="nav">
-            <ul>
-            <li>about</li>
-            <li>hrporko</li>
-            <li>rhrl</li>
+
+export default function Header() {
+  return (
+    <div className="header">
+
+      <div className="nav">
+       
+        <ul>
+           <i class="fas fa-bolt icon1"></i>
+            <li><a href="">Home</a></li> 
+            <li><a href="">about</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Testimonials</a></li>
+            <li><a href="">Team</a></li>
+            <li><a href="">Menu</a></li>
+            <li><a href="">News</a></li>
             </ul>
            </div>
-        </div>
-    )
+<div >
+             <button className="btn-quotes">Get Quotes</button>
+           </div>
+           <div>
+             <button  className="mode"><i class="fas fa-bolt icon"></i></button>
+           </div>
+           
+      
+    </div>
+  );
 }
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import './home.css'
+import './home.css';
 import Header from "../../composant/Header/header";
-export default function Home(){
-    return(
-<>
-        <div className="Ho">
-            <Header/>
+
+export default function Home() {
+  return (
+    <>
+      <div className="home">
+        <Header />
+        <div className="welcome">
+            <p className="pp"> Bienvenue dans notre site</p>
+         
+          <p className="ItS">IT Solutions.
+       </p>
+       <p>You can easily change any design to your own. It is also highly customizable SEO friendly template.</p>
         </div>
-        <div>fiohff</div>
-        </>
-        
-        
-    )
+      </div>
+    </>
+  );
 }
