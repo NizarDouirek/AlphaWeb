2882f3a4113fef589200d35e9474513b98c13ef3
douireknizar <douireknizar@gmail.com>
2025-05-28 17:45:42 +0200

    Add service

--- src/composant/Header/header.jsx
+++ src/composant/Header/header.jsx
@@ -1,32 +1,39 @@
+import { useEffect, useState } from 'react';
 import React from "react";
 import './header.css';
 
 export default function Header() {
+   const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <div className="header">
+    <div className={`header ${isScrolled ? "scrolled" : ""}`}>
+      {/* <img src="RIMBERIO.svg" className="logo" alt="Logo" />. */}
 
       <div className="nav">
-       
         <ul>
-           <i class="fas fa-bolt icon1"></i>
-            <li><a href="">Home</a></li> 
-            <li><a href="">about</a></li>
-            <li><a href="">Services</a></li>
-            <li><a href="">Portfolio</a></li>
-            <li><a href="">Testimonials</a></li>
-            <li><a href="">Team</a></li>
-            <li><a href="">Menu</a></li>
-            <li><a href="">News</a></li>
-            </ul>
-           </div>
-<div >
-             <button className="btn-quotes">Get Quotes</button>
-           </div>
-           <div>
-             <button  className="mode"><i class="fas fa-bolt icon"></i></button>
-           </div>
-           
-      
+          <li><a href="">Home</a></li> 
+          {/* <li><a href="">About</a></li> */}
+          <li><a href="">Services</a></li>
+          {/* <li><a href="">Portfolio</a></li> */}
+          <li><a href="">Testimonials</a></li>
+          <li><a href="">Team</a></li>
+          <li><a href="">Menu</a></li>
+          <li><a href="">News</a></li>
+        </ul>
+      </div>
+
+      <button className="btn-quotes">Get Quotes</button>
+      <button className="mode">
+        <i className="bx bx-moon icon"></i> {/* bx-moon pour dark mode */}
+      </button>
     </div>
   );
 }
